fcf8ac4893bd56ac86ccb66cf15dc262597bdca5
Hwayyar Goo <hwayyargoo@Hwayyars-MacBook-Pro.local>
2026-05-15 21:43:23 -0400

    add a0.cpp

---
+++ Week2a0/Week2a0/a0.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Week2a0
+//
+//  Created by Hwayyar Goo on 5/15/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "#### Assignment Zero ####\n";
+    std::cout << "Subject and Section:\n";
+    std::cout << "OOP244 NAA\n";
+    std::cout << "Name:\n";
+    std::cout << "Huiya Gu\n";
+    std::cout << "Student Number:\n";
+    std::cout << "113004253\n";
+    std::cout << "Email:\n";
+    std::cout << "hgu22@myseneca.ca\n";
+    std::cout << "Github repository URL: \n";
+    std::cout << "https://github.com/guhuiya0103/OOP244NAA\n";
+    return EXIT_SUCCESS;
+}
